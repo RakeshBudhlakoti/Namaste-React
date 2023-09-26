c3163e0dcdfb33eb97e27043830ce2019f189bbc
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-09-26 07:04:35 +0530

    Episode-1

--- EP-01 Inception/App.js
+++ EP-01 Inception/App.js
@@ -1,4 +1,21 @@
-const heading = React.createElement("h1", {}, "Hello World!");
+/*<div id="parent">
+  <div id="child">
+    <h1>I'm H1 Tag</h1>
+    <h2>I'm H2 Tag</h2>
+  </div>
+</div>;
+*/
+const parent = React.createElement("div",{ id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm H1 Tag"),
+    React.createElement("h2", {}, "I'm H2 Tag"),
+  ])
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "title" }, //attributes
+  "Hello World from React!"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
