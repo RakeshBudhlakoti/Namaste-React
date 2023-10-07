30f2a53a0c50ca16d6b30772ca3392b59df574db
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-07 09:10:16 +0530

    Food App Creation

--- EP-04 Food App/App.js
+++ EP-04 Food App/App.js
@@ -108,17 +108,39 @@ let resObj = {
 };
 const RestaurantCard = (props) => {
   const { resData } = props; // destructuring props
-  const {cloudinaryImageId,name,areaName,cuisines,costForTwo,avgRating,totalRatingsString,sla} = resData?.info;
-  
+  const {
+    cloudinaryImageId,
+    name,
+    areaName,
+    cuisines,
+    costForTwo,
+    avgRating,
+    totalRatingsString,
+    sla,
+  } = resData?.info;
+
   return (
     <div className="card">
-      <img className="cardImg" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt="Restaurant Image" />
+      <img
+        className="cardImg"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+        alt="Restaurant Image"
+      />
       <div className="container">
-        <h4>  <b>{name}</b></h4>
-        <h5> {areaName} (Delivery in : {sla.slaString})</h5>
+        <h4>
+          <b>{name}</b>
+        </h4>
+        <h5>
+          {areaName} (Delivery in : {sla.slaString})
+        </h5>
         <h5>{cuisines.join(", ")}</h5>
         <h5>{costForTwo}</h5>
-        <h5> {avgRating} ({totalRatingsString})</h5>
+        <h5>
+          {avgRating} ({totalRatingsString})
+        </h5>
       </div>
     </div>
   );
@@ -136,10 +158,9 @@ const Body = () => {
             <div className="res-container">
               <div className="res-header">Restaurant List</div>
               <div className="res-body">
-              {
-                data.resData.map((restaurant) => <RestaurantCard resData={restaurant} />
-                )
-              }
+                {data.resData.map((restaurant) => (
+                  <RestaurantCard resData={restaurant} />
+                ))}
               </div>
               <div className="res-footer"></div>
             </div>
