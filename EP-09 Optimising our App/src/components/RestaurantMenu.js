85d2af9602ef649ea1991a1749732be3fc5ddc02
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-16 13:55:01 +0530

    Custom Hook Added

--- EP-09 Optimising our App/src/components/RestaurantMenu.js
+++ EP-09 Optimising our App/src/components/RestaurantMenu.js
@@ -1,22 +1,52 @@
 import { CDN_URL, RESTRA_CDN_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams, Link } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import vegImage from '../images/veg.jpeg';
+import nonVegImage from '../images/non-veg.jpeg';
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
-  const resInfo = useRestaurantMenu(resId); // Custom hook for RestaurantMenu API calls
+  const resInfo = useRestaurantMenu(resId);
+
+  const [isVeg, setIsVeg] = useState(true); // State for Veg/Non-Veg toggle
+  const [itemCards, setItemCards] = useState(null); // State for restaurant menu items
+  const [itemCardsFiltered, setItemCardsFiltered] = useState(null); // State for restaurant menu items cloned for fiterator
+
+  console.log(isVeg);
+  useEffect(() => {
+    // Fetch and set menu items when `resInfo` changes
+    if (resInfo) {
+      const itemCardsData =
+        resInfo.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+          ?.card.itemCards ||
+        resInfo.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
+          ?.card.itemCards;
+      setItemCards(itemCardsData);
+      setItemCardsFiltered(itemCardsData);
+    }
+  }, [resInfo]);
+
+  const handleToggle = () => {
+    setIsVeg(!isVeg); // Toggle Veg/Non-Veg state
+    console.log("isVeg " + isVeg);
+    if (isVeg) {
+      setItemCardsFiltered(
+        itemCards.filter((item) => item.card.info.isVeg != 1)
+      );
+    } else {
+      setItemCardsFiltered(
+        itemCards.filter((item) => item.card.info.isVeg === 1)
+      );
+    }
+  };
 
   if (resInfo === null) {
-    return <Shimmer />;
+    return <Shimmer />; // Show loading shimmer while data is being fetched
   }
 
-  const restaurantData = resInfo?.cards[0]?.card?.card?.info;
-  const itemCardsData =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-      ?.card.itemCards ||
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
-      ?.card.itemCards;
+  const restaurantData = resInfo.cards[0]?.card?.card?.info;
 
   return (
     <div className="restaurant-page">
@@ -26,7 +56,10 @@ const RestaurantMenu = () => {
 
       <div className="restaurant-info">
         <div className="restaurant-image">
-          <img src={`${CDN_URL}${restaurantData?.cloudinaryImageId}`} alt={restaurantData?.name} />
+          <img
+            src={`${CDN_URL}${restaurantData?.cloudinaryImageId}`}
+            alt={restaurantData?.name}
+          />
         </div>
         <div className="restaurant-details">
           <h1 className="restaurant-name">{restaurantData?.name}</h1>
@@ -37,7 +70,11 @@ const RestaurantMenu = () => {
             <b>Total Ratings:</b> {restaurantData?.totalRatingsString}
           </p>
           <p className="restaurant-delivery-time">
-            <b>Delivery Time:</b> {restaurantData?.sla.slaString} ({(restaurantData?.expectationNotifiers)?restaurantData?.expectationNotifiers[0]?.text : ''})
+            <b>Delivery Time:</b> {restaurantData?.sla.slaString} (
+            {restaurantData?.expectationNotifiers
+              ? restaurantData?.expectationNotifiers[0]?.text
+              : ""}
+            )
           </p>
           <p className="restaurant-cuisines">
             <b>Cuisines:</b> {restaurantData?.cuisines?.join(", ")}
@@ -46,23 +83,45 @@ const RestaurantMenu = () => {
             <b>Cost for Two:</b> {restaurantData?.costForTwoMessage}
           </p>
           <p className="restaurant-address">
-            <b>Address:</b> {restaurantData?.labels[1]?.message}, {restaurantData?.city}
+            <b>Address:</b> {restaurantData?.labels[1]?.message},{" "}
+            {restaurantData?.city}
           </p>
         </div>
       </div>
 
       <div className="restaurant-menu">
         <h2 className="menu-heading">Menu</h2>
+        <div className="veg-non-veg-slider">
+          <label className={`option ${isVeg ? "selected" : ""}`}>Veg</label>
+          <div className="slider" onClick={handleToggle}>
+            <div className={`slider-knob ${isVeg ? "veg" : "non-veg"}`}></div>
+          </div>
+          <label className={`option ${isVeg ? "" : "selected"}`}>Non-Veg</label>
+        </div>
         <ul className="menu-list">
-          {itemCardsData?.map((item) => (
+          {itemCardsFiltered?.map((item) => (
             <li key={item.card.info.id} className="menu-item">
-              <img src={`${RESTRA_CDN_URL}${item.card.info.imageId}`} alt={item.card.info.name} className="menu-item-image" />
+              <img
+                src={`${RESTRA_CDN_URL}${item.card.info.imageId}`}
+                alt={item.card.info.name}
+                className="menu-item-image"
+              />
               <div className="menu-item-details">
                 <h3 className="item-name">{item.card.info.name}</h3>
                 <p className="item-description">{item.card.info.description}</p>
                 <span className="item-price">
-                  ₹{item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice / 100}
+                  ₹
+                  {item.card.info.price
+                    ? item.card.info.price / 100
+                    : item.card.info.defaultPrice / 100}
                 </span>
+                <p>
+                  <span>
+                   
+                   {item.card.info.isVeg ?  <img className="veg-image" src={vegImage} alt=""/> :  <img className="nonveg-image" src={nonVegImage} alt=""/>}
+
+                  </span>
+                </p>
               </div>
             </li>
           ))}
