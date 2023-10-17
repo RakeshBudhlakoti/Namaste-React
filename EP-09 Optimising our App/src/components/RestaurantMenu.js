6527e4c2440b3376a6822c07447372175e8f780b
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-17 17:17:59 +0530

    Cart Functionality Added

--- EP-09 Optimising our App/src/components/RestaurantMenu.js
+++ EP-09 Optimising our App/src/components/RestaurantMenu.js
@@ -14,6 +14,8 @@ const RestaurantMenu = () => {
   const [itemCardsFiltered, setItemCardsFiltered] = useState([]);
   const [filter, setFilter] = useState("all");
 
+  const [counters, setCounters] = useState({}); // Separate counts for each item
+
   useEffect(() => {
     if (resInfo) {
       const itemCardsData =
@@ -42,6 +44,35 @@ const RestaurantMenu = () => {
     }
   };
 
+  const handleAddClick = (itemId) => {
+    const newCounters = { ...counters };
+    newCounters[itemId] = (newCounters[itemId] || 0) + 1;
+    setCounters(newCounters);
+  };
+
+  const handleDecrementClick = (itemId) => {
+    if (counters[itemId] > 0) {
+      const newCounters = { ...counters };
+      newCounters[itemId] = newCounters[itemId] - 1;
+      setCounters(newCounters);
+    }
+  };
+  let cartItems = [];
+
+  itemCardsFiltered.forEach((item) => {
+    const itemId = item.card.info.id;
+    if (counters[itemId]) {
+      const availableItem = {
+        name: item.card.info.name,
+        price: item.card.info.finalPrice || item.card.info.price,
+        count: counters[itemId],
+      };
+      cartItems.push(availableItem);
+    }
+  });
+
+  console.log("cartItems ", cartItems);
+
   if (resInfo === null) {
     return <Shimmer />;
   }
@@ -50,10 +81,6 @@ const RestaurantMenu = () => {
 
   return (
     <div className="restaurant-page">
-      <Link className="previous" to="/">
-        &laquo; Back
-      </Link>
-
       <div className="restaurant-info">
         <div className="restaurant-image">
           <img
@@ -88,61 +115,125 @@ const RestaurantMenu = () => {
           </p>
         </div>
       </div>
-
-      <div className="restaurant-menu">
-        <h2 className="menu-heading">Menu</h2>
-        <div className="toggle-buttons">
-          <button
-            className={`toggle-button ${filter === "all" ? "selected" : ""}`}
-            onClick={() => handleToggle("all")}
-          >
-            All
-          </button>
-          <button
-            className={`toggle-button ${filter === "veg" ? "selected" : ""}`}
-            onClick={() => handleToggle("veg")}
-          >
-            Veg
-          </button>
-          <button
-            className={`toggle-button ${
-              filter === "non-veg" ? "selected" : ""
-            }`}
-            onClick={() => handleToggle("non-veg")}
-          >
-            Non-Veg
-          </button>
-        </div>
-        <ul className="menu-list">
-          {itemCardsFiltered?.map((item) => (
-            <li key={item.card.info.id} className="menu-item">
-              <img
-                src={`${RESTRA_CDN_URL}${item.card.info.imageId}`}
-                alt={item.card.info.name}
-                className="menu-item-image"
-              />
-              <div className="menu-item-details">
-                <h3 className="item-name">{item.card.info.name}</h3>
-                <p className="item-description">{item.card.info.description}</p>
-                <span className="item-price">
-                  ₹
-                  {item.card.info.price
-                    ? item.card.info.price / 100
-                    : item.card.info.defaultPrice / 100}
-                </span>
-                <p>
-                  <span>
-                    {item.card.info.isVeg ? (
-                      <img className="veg-image" src={vegImage} alt="" />
-                    ) : (
-                      <img className="nonveg-image" src={nonVegImage} alt="" />
-                    )}
+      <div className="content-wrapper">
+        <div className="restaurant-menu">
+          <h2 className="menu-heading">Menu</h2>
+          <div className="toggle-buttons">
+            <button
+              className={`toggle-button ${filter === "all" ? "selected" : ""}`}
+              onClick={() => handleToggle("all")}
+            >
+              {" "}
+              All{" "}
+            </button>
+            <button
+              className={`toggle-button ${filter === "veg" ? "selected" : ""}`}
+              onClick={() => handleToggle("veg")}
+            >
+              {" "}
+              Veg{" "}
+            </button>
+            <button
+              className={`toggle-button ${
+                filter === "non-veg" ? "selected" : ""
+              }`}
+              onClick={() => handleToggle("non-veg")}
+            >
+              {" "}
+              Non-Veg{" "}
+            </button>
+          </div>
+          <ul className="menu-list">
+            {itemCardsFiltered?.map((item) => (
+              <li key={item.card.info.id} className="menu-item">
+                <img
+                  src={`${RESTRA_CDN_URL}${item.card.info.imageId}`}
+                  alt={item.card.info.name}
+                  className="menu-item-image"
+                />
+                <div className="menu-item-details">
+                  <h3 className="item-name">{item.card.info.name}</h3>
+                  <p className="item-description">
+                    {item.card.info.description}
+                  </p>
+                  <span className="item-price">
+                    ₹
+                    {item.card.info.price
+                      ? item.card.info.price / 100
+                      : item.card.info.defaultPrice / 100}
                   </span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+                  <p>
+                    <span>
+                      {item.card.info.isVeg ? (
+                        <img className="veg-image" src={vegImage} alt="" />
+                      ) : (
+                        <img
+                          className="nonveg-image"
+                          src={nonVegImage}
+                          alt=""
+                        />
+                      )}
+                    </span>
+                  </p>
+                </div>
+                <div className="menu-item-count">
+                  <div className="button-counter">
+                    {!counters[item.card.info.id] ? (
+                      <button onClick={() => handleAddClick(item.card.info.id)}>
+                        ADD
+                      </button>
+                    ) : (
+                      <div className="counter">
+                        <button
+                          onClick={() =>
+                            handleDecrementClick(item.card.info.id)
+                          }
+                        >
+                          -
+                        </button>
+                        <span>{counters[item.card.info.id]}</span>
+                        <button
+                          onClick={() => handleAddClick(item.card.info.id)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="cart-sidebar">
+          <div className="cart-header">
+            <h2>Your Cart</h2>
+            <button className="close-button">&times;</button>
+          </div>
+          <div className="cart-items">
+            <table className="cart-table">
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id} className="cart-item">
+                    <td>{item.name}</td>
+                    <td>₹{(item.price / 100) * item.count}</td>
+                    <td>{item.count}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="cart-total">
+            <h3>Go To Cart</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
