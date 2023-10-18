d88016aadd0d139f692c11c62b7118097c8fd489
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-18 17:34:09 +0530

    New Changes

---
+++ EP-11 Data in the new oil/src/components/Header.js
@@ -0,0 +1,48 @@
+import { LOGO_URL } from "../utils/constants";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+function Header() {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+
+  const handleLoginClick = () => {
+    setBtnNameReact(btnNameReact === "Login" ? "Logout" : "Login");
+  };
+
+  const onlineStatus = useOnlineStatus(); // Custom Hook for online status
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} alt="FoodVillage" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            Online Status:{onlineStatus ? <span>&#128994;</span> : <span>&#128308;</span>}
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li>
+            <button className="login" onClick={handleLoginClick}>
+              {btnNameReact}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ EP-11 Data in the new oil/src/components/RestaurantCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { CDN_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const RestaurantCard = ({ resData }) => {
+  if (!resData) {
+    return null; // Handle the case where resData is not available
+  }
+
+  const {
+    id,
+    cloudinaryImageId,
+    name,
+    areaName,
+    cuisines,
+    costForTwo,
+    avgRating,
+    totalRatingsString,
+    sla,
+  } = resData.info || {};
+
+  const truncatedCuisines = cuisines.join(", ").substring(0, 20) + "...";
+
+  return (
+    <div className="card">
+      <Link to={"/restaurants/" + id}>
+        <div className="cardBody">
+          <img
+            className="cardImg"
+            src={`${CDN_URL}${cloudinaryImageId}`}
+            alt="Restaurant Image"
+          />
+
+          <h4>
+            <b>{name}</b>
+          </h4>
+          <h5>{`${areaName} (${sla.slaString})`}</h5>
+          <h5>{truncatedCuisines}</h5>
+          <span className="allDescription">
+            <p>{(avgRating >= 4) ? (<span className="rating">{avgRating}</span>) :(<span className="rating1">{avgRating}</span>)}</p>
+            <p>•</p>
+            <p>{sla.lastMileTravelString}</p>
+            <p>•</p>
+            <p>{costForTwo}</p>
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default RestaurantCard;
